fd8b526c0d88a9c3e9c671146507ff383a20e2a1
Felipe Rodrigues <felipe.rodrigues@keyrus.com.br>
2019-03-06 01:48:26 -0300

    implements hystrix command fallback config

--- src/main/java/br/com/circuitbreaker/hystrix/controller/CepController.java
+++ src/main/java/br/com/circuitbreaker/hystrix/controller/CepController.java
@@ -1,6 +1,7 @@
 package br.com.circuitbreaker.hystrix.controller;
 
 import br.com.circuitbreaker.hystrix.dto.CepResponse;
+import br.com.circuitbreaker.hystrix.feign.CepService;
 import br.com.circuitbreaker.hystrix.impl.CepServiceFallback;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,9 +16,26 @@ public class CepController {
     @Autowired
     private CepServiceFallback cepServiceFallback;
 
+    @Autowired
+    private CepService cepService;
+
+    //feature 1
     @GetMapping("/cep/{cep}")
     public CepResponse cep(@PathVariable("cep") String cep)
     {
+        return cepServiceFallback.consultCepValue(cep);
+    }
+
+
+    //feature 2
+    @GetMapping("/cep2/{cep}")
+    public CepResponse cep2(@PathVariable("cep") String cep)
+    {
+        CepResponse cepResponse = cepService.getCep2(cep).execute();
+        if(cepResponse != null)
+        {
+            return cepResponse;
+        }
         return cepServiceFallback.consultCepValue(cep);
     }
 
--- src/main/java/br/com/circuitbreaker/hystrix/feign/CepService.java
+++ src/main/java/br/com/circuitbreaker/hystrix/feign/CepService.java
@@ -2,6 +2,8 @@ package br.com.circuitbreaker.hystrix.feign;
 
 
 import br.com.circuitbreaker.hystrix.dto.CepResponse;
+import com.netflix.hystrix.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,7 +11,14 @@ import org.springframework.web.bind.annotation.PathVariable;
 @FeignClient(name = "cepService" , url = "http://api.postmon.com.br")
 public interface CepService {
 
+    //feature 1
     @GetMapping("/v1/cep/{cep}")
     CepResponse getCep(@PathVariable("cep") String cep);
+
+    //feature 2
+    @GetMapping(value = "/v1/cep/{cep}" )
+    @HystrixProperty(name ="hystrix.command.default.execution.timeout.enabled", value = "false")
+    HystrixCommand<CepResponse> getCep2(@PathVariable("cep") String cep);
+
 
 }
